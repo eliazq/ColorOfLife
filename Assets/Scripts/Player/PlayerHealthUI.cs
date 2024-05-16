b96990cc4cde6849cd12fd71b8cec4c875ced61b
Elias Heijari <69460338+eliazq@users.noreply.github.com>
2024-05-16 18:14:30 +0300

    Update health on event

--- Assets/Scripts/Player/PlayerHealthUI.cs
+++ Assets/Scripts/Player/PlayerHealthUI.cs
@@ -8,7 +8,17 @@ public class PlayerHealthUI : MonoBehaviour
     [SerializeField] private Image healthBarImage; // 0.625 max fill amount
     private float maxFillAmount = 0.625f;
 
-    private void Update()
+    private void Start()
+    {
+        Player.Instance.OnDamageTaken += Instance_OnDamageTaken;
+    }
+
+    private void Instance_OnDamageTaken(object sender, System.EventArgs e)
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
     {
         float healthPercentage = (float)Player.Instance.health / Player.Instance.maxHealth;
         healthBarImage.fillAmount = healthPercentage * maxFillAmount;
