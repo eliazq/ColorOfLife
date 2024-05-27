a83a0d6cad60cc9f6206fd198cfa5a96ad40e77b
aleksizzzz <122257914+aleksizzzz@users.noreply.github.com>
2024-05-27 22:47:38 +0300

    SKYBOX AND POST PROCESS

---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    public float SkyBoxRotateSpeed = 1.2f;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * SkyBoxRotateSpeed);;
+    }
+}
