52023aae9bb639b91f42198d91259d01ddd681b6
aleksizzzz <122257914+aleksizzzz@users.noreply.github.com>
2024-06-09 00:20:50 +0300

    checkpoint sound when trigger

--- Assets/Scripts/CheckPoint/CheckPoint.cs
+++ Assets/Scripts/CheckPoint/CheckPoint.cs
@@ -6,11 +6,14 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private GameObject notCollected;
     [SerializeField] private GameObject collected;
+    [SerializeField] private AudioSource CheckpointClip;
 
     private void OnTriggerEnter(Collider other)
     {
         Player.Instance.spawnPosition = transform.position;
         notCollected.SetActive(false);
         collected.SetActive(true);
+        CheckpointClip.Play();
+
     }
 }
