ab4bd1399e965e544c7f2e80a145cdf91bb21732
Elias Heijari <69460338+eliazq@users.noreply.github.com>
2024-06-09 13:21:01 +0300

    Shows Guide UI After Time

---
+++ Assets/Scripts/HideAfterTime.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAfterTime : MonoBehaviour
+{
+    [SerializeField] private float time = 8f;
+    void Start()
+    {
+        StartCoroutine(HideAfterTimeCoroutine());
+    }
+
+    IEnumerator HideAfterTimeCoroutine()
+    {
+        yield return new WaitForSeconds(time);
+        StartCoroutine(ShowAfterTime());
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator ShowAfterTime()
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(true);
+    }
+
+}
