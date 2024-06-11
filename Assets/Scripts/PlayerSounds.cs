fc1b36b1bf42f31e8871f52b4e82fe8a7542177c
Elias Heijari <69460338+eliazq@users.noreply.github.com>
2024-06-11 14:05:00 +0100

    Camera Glitch Fix And Volumes

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -15,7 +15,6 @@ public class PlayerSounds : MonoBehaviour
 
     private void VThirdPersonController_OnLandedGround(object sender, vThirdPersonMotor.YVelocityEventArgs e)
     {
-
         SoundManager.Sound[] soundList = new SoundManager.Sound[] { SoundManager.Sound.LandToGround1, SoundManager.Sound.LandToGround2, SoundManager.Sound.LandToGround3, };
         int ranNum = Random.Range(0, soundList.Length);
         SoundManager.PlaySound(soundList[ranNum], Player.Instance.transform.position);
